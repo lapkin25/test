e533d3c546ebe4bc628011ac43eed6b3b9b16d11
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2025-09-20 14:19:10 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const s = require('http').Server((req, res) => {
+   let url = req.url;
+   if (url.slice(0, 5) == "/deg/") {
+      let str = url.slice(5); 
+      let arr = str.split("/");
+      let x1 = arr[0];
+      let x2 = arr[1];
+      let ans = Math.pow(x1, x2);
+   }
+   res.end(ans);
+});
+s.listen(4321);
