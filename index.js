1f02bc7ebab2e5751eaa8cebd5e1c9f8000d829b
Yuly Lapkin <127262950+lapkin25@users.noreply.github.com>
2025-09-20 14:40:19 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const s = require('http').Server((req, res) => {
+const { createReadStream } = require('fs');
+
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept, Access-Control-Allow-Headers'
+};
+
+require ('http')
+.Server((req, res) => {
    let url = req.url;
    if (url.slice(0, 5) == "/deg/") {
       let str = url.slice(5); 
@@ -8,7 +17,13 @@ const s = require('http').Server((req, res) => {
       let ans = Math.pow(x1, x2);
       res.end(ans);
    }
-   else if (url == "/login" || url == "/login/")
-      res.end("grenkingv");
-});
-s.listen(4321);
+
+
+  if (req.url === '/login/') {
+    res.writeHead(200, '', CORS);
+    return res.end(process.env.login);
+  }  
+
+ 
+})
+.listen(process.env.PORT);
